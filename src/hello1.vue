05a2fe074ad8a52d624e21ca3e4635ad43d605b7
cym <519338858@qq.com>
2018-01-04 12:51:53 +0800

    123

---
+++ src/hello1.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="hello1">
+		<image :src="logoUrl" class="logo"></image>
+		<text class="title">Hello {{target}}</text>
+	</div>
+	
+</template>
+
+<script>
+	
+	export default {
+		
+		data:{
+			logoUrl: 'https://www.baidu.com/img/bd_logo1.png',
+			target:"world 1.vue"
+			
+		}
+		
+	}
+	
+</script>
+
+<style>
+	.wrapper { align-items: center; margin-top: 120px; }
+	.title { padding-top:40px; padding-bottom: 40px; font-size: 48px; }
+	.logo { width: 300px; height: 150px; }
+	
+</style>
---
+++ src/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="wrapper" @click="update">
+    <image :src="logoUrl" class="logo"></image>
+    <text class="title">Hello {{target}}</text>
+    <text class="desc">Now, let's use vue to build your weex app.</text>
+    <button type="button" class="redirectButton" @click="redirect">Redirect</button>
+  </div>
+</template>
+
+<style>
+  .wrapper { align-items: center; margin-top: 120px; }
+  .title { padding-top:40px; padding-bottom: 40px; font-size: 48px; }
+  .logo { width: 360px; height: 156px; }
+  .desc { padding-top: 20px; color:#888; font-size: 24px;}
+  .redirectButton {width:300px;height:150px;font-size: 32px;text-align:center; margin:50px;}
+</style>
+
+<script>
+  export default {
+    data: {
+      logoUrl: 'https://www.baidu.com/img/bd_logo1.png',
+      target: 'World index.vue'
+    },
+    methods: {
+      update: function (e) {
+        this.target = 'Weex'
+        console.log('target:', this.target)
+      },
+      
+      redirect:function(e) {
+      	window.location.href='https://www.baidu.com';
+      }
+      
+      
+    }
+  }
+</script>
